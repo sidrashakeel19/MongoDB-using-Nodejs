388cdaa8fe090ba93ecd6af12a07cfb53e5e6429
home <sidrashakeel19@hotmail.com>
2020-08-17 22:45:44 +0500

    Deleted multiple docs

--- deletedoc_demo.js
+++ deletedoc_demo.js
@@ -1,5 +1,22 @@
-// Delete one
-// Delete the document with the address "Mountain 21":
+// // Delete one
+// // Delete the document with the address "Mountain 21":
+
+// var MongoClient = require('mongodb').MongoClient;
+// var url = "mongodb://localhost:27017/";
+
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("mydb");
+//   var myquery = { address: 'Mountain 21' };
+//   dbo.collection("customers").deleteOne(myquery, function(err, obj) {
+//     if (err) throw err;
+//     console.log("1 document deleted");
+//     db.close();
+//   });
+// });
+
+// Delete many
+// Delete all documents were the address starts with the letter "O":
 
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
@@ -7,10 +24,10 @@ var url = "mongodb://localhost:27017/";
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("mydb");
-  var myquery = { address: 'Mountain 21' };
-  dbo.collection("customers").deleteOne(myquery, function(err, obj) {
+  var myquery = { address: /^O/ };
+  dbo.collection("customers").deleteMany(myquery, function(err, obj) {
     if (err) throw err;
-    console.log("1 document deleted");
+    console.log(obj.result.n + " document(s) deleted");
     db.close();
   });
 });
