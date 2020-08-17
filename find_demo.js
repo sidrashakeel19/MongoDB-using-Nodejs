e18a93f1c6f3a53463bd3ab84e29e8938e359603
home <sidrashakeel19@hotmail.com>
2020-08-17 22:19:38 +0500

    Find all documents

--- find_demo.js
+++ find_demo.js
@@ -1,5 +1,20 @@
-// Find one
-// This will return first document from database
+// // Find one
+// // This will return first document from database
+
+// var MongoClient = require('mongodb').MongoClient;
+// var url = "mongodb://localhost:27017/";
+
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("mydb");
+//   dbo.collection("customers").findOne({}, function(err, result) {
+//     if (err) throw err;
+//     console.log(result.name);
+//     db.close();
+//   });
+// });
+
+// Find multiple
 
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
@@ -7,9 +22,9 @@ var url = "mongodb://localhost:27017/";
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("mydb");
-  dbo.collection("customers").findOne({}, function(err, result) {
+  dbo.collection("customers").find({}).toArray(function(err, result) {
     if (err) throw err;
-    console.log(result.name);
+    console.log(result);
     db.close();
   });
 });
