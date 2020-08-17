c548ea45c423058a518e4f943243cb2b73f2e912
home <sidrashakeel19@hotmail.com>
2020-08-17 22:52:31 +0500

    Drop collection

---
+++ deletecollection_demo.js
@@ -0,0 +1,31 @@
+// // You can delete a table, or collection as it is called in MongoDB, by using the drop() method.
+
+// var MongoClient = require('mongodb').MongoClient;
+// var url = "mongodb://localhost:27017/";
+
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("mydb");
+//   dbo.collection("customers").drop(function(err, delOK) {
+//     if (err) throw err;
+//     if (delOK) console.log("Collection deleted");
+//     db.close();
+//   });
+// });
+
+// You can also use the dropCollection() method to delete a table (collection).
+
+// The dropCollection() method takes two parameters: the name of the collection and a callback function.
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  dbo.dropCollection("customers", function(err, delOK) {
+    if (err) throw err;
+    if (delOK) console.log("Collection deleted");
+    db.close();
+  });
+});
