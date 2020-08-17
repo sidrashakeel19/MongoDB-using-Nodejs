10eac7793e7475afdc7948d96464f5c25774d110
home <sidrashakeel19@hotmail.com>
2020-08-17 22:41:36 +0500

    Update many docs

--- update_demo.js
+++ update_demo.js
@@ -1,5 +1,22 @@
-// Update one
-// Update the document with the address "Valley 345" to name="Mickey" and address="Canyon 123":
+// // Update one
+// // Update the document with the address "Valley 345" to name="Mickey" and address="Canyon 123":
+
+// var MongoClient = require('mongodb').MongoClient;
+// var url = "mongodb://127.0.0.1:27017/";
+
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("mydb");
+//   var myquery = { address: "Valley 345" };
+//   var newvalues = { $set: {name: "Mickey", address: "Canyon 123" } };
+//   dbo.collection("customers").updateOne(myquery, newvalues, function(err, res) {
+//     if (err) throw err;
+//     console.log("1 document updated");
+//     db.close();
+//   });
+// });
+
+// Update many
 
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://127.0.0.1:27017/";
@@ -7,11 +24,11 @@ var url = "mongodb://127.0.0.1:27017/";
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("mydb");
-  var myquery = { address: "Valley 345" };
-  var newvalues = { $set: {name: "Mickey", address: "Canyon 123" } };
-  dbo.collection("customers").updateOne(myquery, newvalues, function(err, res) {
+  var myquery = { address: /^S/ };
+  var newvalues = {$set: {name: "Minnie"} };
+  dbo.collection("customers").updateMany(myquery, newvalues, function(err, res) {
     if (err) throw err;
-    console.log("1 document updated");
+    console.log(res.result.nModified + " document(s) updated");
     db.close();
   });
 });
